cf7cd1f2be98fdd953f7df3d48fc82f6c8564014
Maelle05 <maelle.rabouan@gmail.com>
2023-03-02 18:20:19 +0100

    feat(): add cursor brush

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ import WebGlManager from "./webgl.js";
 const webgl = new WebGlManager(document.querySelector('#webgl'));
 let mouse = 'up';
 
+const cursor = document.querySelector('#cursor')
+
 window.addEventListener('resize', () => webgl.resize())
 window.addEventListener('mousemove', (e) => {
+  cursor.style.top = `calc(${e.clientY}px - 10px)`
+  cursor.style.left = `calc(${e.clientX}px - 10px)`
   if(mouse === 'down') webgl.updateMouse(e)
 })
 window.addEventListener('mouseup', () => {
@@ -13,6 +17,15 @@ window.addEventListener('mouseup', () => {
 window.addEventListener('mousedown', () => {
   webgl.resetMouse()
   mouse = 'down'
+})
+
+document.querySelectorAll('a').forEach((link)=>{
+  link.addEventListener('mouseover', ()=>{
+    cursor.style.opacity = '0'
+  })
+  link.addEventListener('mouseout', ()=>{
+    cursor.style.opacity = '1'
+  })
 })
 
 const tick = () =>
