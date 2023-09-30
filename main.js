cfa93a24bb2f7b132adfcb84a9eece4242c1e466
Maelle05 <maelle.rabouan@gmail.com>
2023-09-30 19:20:14 +0200

    feat(): add waterfall

--- main.js
+++ main.js
@@ -53,17 +53,17 @@ const projects = document.querySelectorAll('#second a')
 projects.forEach((project)=>{
   project.addEventListener('mouseover', ()=>{
     projects.forEach((e)=>{
-      e.style.filter = "grayscale(" + 100 + "%)"
+      // e.style.filter = "grayscale(" + 100 + "%)"
       e.querySelector('video').currentTime = 0
       e.querySelector('video').pause()
     })
     // project.style.opacity = '1'
-    project.style.filter = "grayscale(" + 0 + "%)"
+    // project.style.filter = "grayscale(" + 0 + "%)"
     project.querySelector('video').play()
   })
   project.addEventListener('mouseout', ()=>{
     projects.forEach((e)=>{
-      e.style.filter = "grayscale(" + 0 + "%)"
+      // e.style.filter = "grayscale(" + 0 + "%)"
       // e.style.opacity = '1'
       e.querySelector('video').play()
     })
