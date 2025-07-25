c7acc877113197e5684c9f88da4ae7e69d4107e4
Maelle05 <maelle.rabouan@gmail.com>
2025-07-25 11:16:10 +0200

    clean() remove webgl + feat() add links & projects

--- main.js
+++ main.js
@@ -1,98 +1,99 @@
 import WebGlManager from "./webgl.js";
-let webgl = null
+let webgl = null;
 
 /* Storing user's device details in a variable*/
 let details = navigator.userAgent;
-  
+
 /* Creating a regular expression 
 containing some mobile devices keywords 
 to search it in details string*/
 let regexp = /android|iphone|kindle|ipad/i;
-  
+
 /* Using test() method to search regexp in details
 it returns boolean value*/
 let isMobileDevice = regexp.test(details);
-  
+
 if (isMobileDevice) {
-    console.log("Mobile 📱");
+  console.log("Mobile 📱");
 } else {
-    console.log("Desktop 💻");
+  console.log("Desktop 💻");
 
-    webgl = new WebGlManager(document.querySelector('#webgl'));
+  // webgl = new WebGlManager(document.querySelector("#webgl"));
 
-    const cursor = document.querySelector('#cursor')
+  // const cursor = document.querySelector("#cursor");
 
-    window.addEventListener('resize', () => webgl.resize())
-    window.addEventListener('pointermove', (e) => {
-      cursor.style.top = `calc(${e.clientY + document.documentElement.scrollTop}px - 10px)`
-      cursor.style.left = `calc(${e.clientX}px - 10px)`
-      webgl.updateMouse(e)
-    })
+  // window.addEventListener("resize", () => webgl.resize());
+  // window.addEventListener("pointermove", (e) => {
+  //   cursor.style.top = `calc(${
+  //     e.clientY + document.documentElement.scrollTop
+  //   }px - 10px)`;
+  //   cursor.style.left = `calc(${e.clientX}px - 10px)`;
+  //   webgl.updateMouse(e);
+  // });
 
-    document.querySelectorAll('a').forEach((link)=>{
-      link.addEventListener('mouseover', ()=>{
-        cursor.style.opacity = '0'
-      })
-      link.addEventListener('mouseout', ()=>{
-        cursor.style.opacity = '1'
-      })
-    })
+  // document.querySelectorAll("a").forEach((link) => {
+  //   link.addEventListener("mouseover", () => {
+  //     cursor.style.opacity = "0";
+  //   });
+  //   link.addEventListener("mouseout", () => {
+  //     cursor.style.opacity = "1";
+  //   });
+  // });
 
-    document.querySelectorAll('#scroll').forEach((link)=>{
-      link.addEventListener('mouseover', ()=>{
-        cursor.style.opacity = '0'
-      })
-      link.addEventListener('mouseout', ()=>{
-        cursor.style.opacity = '1'
-      })
-    })
+  // document.querySelectorAll("#scroll").forEach((link) => {
+  //   link.addEventListener("mouseover", () => {
+  //     cursor.style.opacity = "0";
+  //   });
+  //   link.addEventListener("mouseout", () => {
+  //     cursor.style.opacity = "1";
+  //   });
+  // });
 }
 if (isMobileDevice) {
-  const projects = document.querySelectorAll('#second a')
+  const projects = document.querySelectorAll("#second a");
 
-  projects.forEach((project)=>{
-    project.addEventListener('mouseover', ()=>{
-      projects.forEach((e)=>{
+  projects.forEach((project) => {
+    project.addEventListener("mouseover", () => {
+      projects.forEach((e) => {
         // e.style.filter = "grayscale(" + 100 + "%)"
-        e.querySelector('video').currentTime = 0
-        e.querySelector('video').pause()
-      })
+        e.querySelector("video").currentTime = 0;
+        e.querySelector("video").pause();
+      });
       // project.style.opacity = '1'
       // project.style.filter = "grayscale(" + 0 + "%)"
-      project.querySelector('video').play()
-    })
-    project.addEventListener('mouseout', ()=>{
-      projects.forEach((e)=>{
+      project.querySelector("video").play();
+    });
+    project.addEventListener("mouseout", () => {
+      projects.forEach((e) => {
         // e.style.filter = "grayscale(" + 0 + "%)"
         // e.style.opacity = '1'
-        e.querySelector('video').play()
-      })
-      project.querySelector('video').currentTime = 0
-    })
-  })
+        e.querySelector("video").play();
+      });
+      project.querySelector("video").currentTime = 0;
+    });
+  });
 }
 
-const tick = () =>
-{
-  if (webgl && webgl.ready) webgl.tick()
+const tick = () => {
+  if (webgl && webgl.ready) webgl.tick();
 
   // Call tick again on the next frame
-  window.requestAnimationFrame(tick)
-}
+  window.requestAnimationFrame(tick);
+};
 
-tick()
+// tick();
 
 // Smooth scroll
 
-const lenis = new Lenis()
+const lenis = new Lenis();
 
-document.querySelector('#scroll').addEventListener('click', () => {
-  lenis.scrollTo(document.querySelector('#second'), { offset: -100})
-})
+document.querySelector("#scroll").addEventListener("click", () => {
+  lenis.scrollTo(document.querySelector("#second"), { offset: -100 });
+});
 
 function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
+  lenis.raf(time);
+  requestAnimationFrame(raf);
 }
 
-requestAnimationFrame(raf)
+requestAnimationFrame(raf);
